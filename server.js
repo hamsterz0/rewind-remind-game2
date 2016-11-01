752016a13a832813e83e2afc58296d99456a5ec5
thearnavgarg <thearnavgarg@gmail.com>
2016-10-31 23:14:05 -0500

    password encrytion done

--- server.js
+++ server.js
@@ -4,14 +4,31 @@ var express         = require('express'),
     path            = require('path'),
     mongoose        = require('mongoose'),
     models          = require('./models/index.js'),
-    bcrypt          = require('bcrypt'),
-    session         = require('client-sessions');
+    session         = require('client-sessions'),
+    crypto          = require('crypto');
+    
 
 
 //configuration
 var port = process.env.PORT || 3000;
+var algorithm       = 'aes-256-ctr',
+    password        = 'd6F3Efeq';;
 mongoose.connect('mongodb://localhost/rewind-remind');
 app.set('view engine', 'ejs');
+
+function encrypt(text){
+  var cipher = crypto.createCipher(algorithm,password)
+  var crypted = cipher.update(text,'utf8','hex')
+  crypted += cipher.final('hex');
+  return crypted;
+}
+ 
+function decrypt(text){
+  var decipher = crypto.createDecipher(algorithm,password)
+  var dec = decipher.update(text,'hex','utf8')
+  dec += decipher.final('utf8');
+  return dec;
+}
 
 //middleware
 app.use(bodyParser());
@@ -36,12 +53,35 @@ app.get('/register', function(req, res) {
     res.render('register.ejs');
 });
 
+app.get('/logout', function(req, res) {
+    req.session.reset();
+    res.redirect('/');
+});
+
 app.get('/dashboard', function(req, res) {
-    res.render('dashboard.ejs');
+    if(req.session && req.session.user) {
+        
+        models.auth.findOne({
+            email: req.session.user
+        }, function(err, user) {
+            if(!user) {
+                req.session.reset();
+                res.redirect('/login');
+            } else {
+                res.locals.user = user;
+                delete res.locals.user.password;
+                res.render('dashboard.ejs');
+            }
+        });
+    } else {
+        res.redirect('/login');
+    }
 });
 
 //----------------POST REQUESTS--------------------
 app.post('/register', function(req, res) {
+
+    req.body.password = encrypt(req.body.password);
     
     var userAuth = new models.auth({
         firstname:  req.body.firstname,
@@ -74,7 +114,7 @@ app.post('/login', function(req, res) {
         if(!user) {
             res.render('login.ejs', {error: 'Incorrect email or password'});
         } else {
-            if(user.password === req.body.password) {
+            if(user.password === encrypt(req.body.password)) {
                 req.session.user = user.email;
                 res.redirect('/dashboard');
             } else {
